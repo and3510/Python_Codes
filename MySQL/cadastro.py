7ab46cb571439e68fa69b15b177b2d0bbdaee287
anderson <and.dovale@gmail.com>
2024-03-01 23:51:18 -0300

    azsad

--- MySQL/cadastro.py
+++ MySQL/cadastro.py
@@ -10,13 +10,13 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 # -------------------------
-name = "Luiz"
-nascimento = "2005-07-20"
-profissao = "TI"
-sexo = "M"
-peso = 75.6
-altura = 2.0
-nacionalidade = "Brazil"
+name = str(input("Digite o nome: "))
+nascimento = str(input("Digite o data de nascimento <YYYY-mm-dd>: "))
+profissao = str(input("Digite a Profissão: "))
+sexo = str(input("Digite (M ou F): "))
+peso = float(input("Digite o peso: "))
+altura = float(input("Digite o altura: "))
+nacionalidade = str(input("Digite a Nacionalidade: "))
 # -------------------------
 
 comando = f'insert into cadastro.pessoas (name, profissao, nascimento, sexo, peso, altura, nacionalidade) values ("{name}", "{profissao}", "{nascimento}", "{sexo}", {peso}, {altura}, "{nacionalidade}")'
